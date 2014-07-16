871dd6d3fc878d635f45713b48a1fe66a612b717
Leslie Lai <leslie.t.lai@gmail.com>
2014-07-15 17:17:00 -0700

    Geocode location and save coordinates when creating a Secret

--- api/controllers/SecretController.js
+++ api/controllers/SecretController.js
@@ -16,8 +16,46 @@
  */
 
 module.exports = {
-    
-  
+  create: function(req, res) {
+    // Replace all non-alphanumeric characters with '+' to form query string
+    var location = req.body['location'].replace(/\W+/g, '+');
+    var params = location + "&key=#{sails.config.geo_api_key}";
+    var GEOCODING_DOMAIN = 'maps.googleapis.com'
+    var https = require('https')
+
+    var options = {
+      hostname: GEOCODING_DOMAIN, // Needs to be bare domain name
+      path: '/maps/api/geocode/json?address=' + params,
+      port: 443 // The API requires https, so that's port 443
+    };
+
+    var geoData = '';
+
+    https.get(options, function(response) {
+      sails.log('response ' + response.statusCode);
+      response.on('error', function(e) { res.send(e.message) });
+      response.on('data', function(chunk) { geoData += chunk });
+      response.on('end', function() {
+        var coordinates = JSON.parse(geoData).results[0].geometry.location;
+        createSecret(coordinates);
+      });
+    });
+
+    function createSecret(coordinates) {
+      Secret.create({
+        text: req.body['text'],
+        location: coordinates,
+      }).done(function(err, secret) {
+        if (err) {
+          res.send(err);
+        }
+        else {
+          res.redirect('/secret/' + secret.id); // is there a better way to do this?
+        }
+      });
+    }
+  },
+
 
 
   /**
